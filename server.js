2ccd49a0235c217ebd5a0578016e2e7b1ba41a61
Matthias Hüsch <matthias@cookiehoarder.com>
2018-09-23 16:40:50 +0200

    setup

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require("hbs");
+const fs = require('fs');
+const port= process.env.port || 3000 ;
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log =`${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile("server.log", log + "\r\n ", (err) => {
+        if (err)
+            console.log('fehler beim speichern');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Kapuut'
+//     })
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {        
+        pageTitle: 'Yo!',
+        welcomeMessage: 'Tach, blabla und überhaupst.'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'about page',
+    })
+});
+
+
+
+app.get('/bad', (req, res) => {
+    res.send({ stuff: "bad stuff", otherstuff: 'even badder'})
+});
+
+app.listen(port, () => {
+    console.log(`Server is open on port ${port}`);
+});
+
